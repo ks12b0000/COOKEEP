b3c11dec4b587f8e8488dab0620ee3e19d1952ad
ChangDaeJun <eowns1209@gmail.com>
2023-01-12 20:50:19 +0900

    댓글 생성일 yyyy-MM-dd 형식으로 응답

--- backend/src/main/java/teamproject/backend/boardComment/dto/BoardCommentResponse.java
+++ backend/src/main/java/teamproject/backend/boardComment/dto/BoardCommentResponse.java
@@ -6,6 +6,7 @@ import lombok.NoArgsConstructor;
 import teamproject.backend.domain.BoardComment;
 
 import javax.persistence.Id;
+import java.text.SimpleDateFormat;
 import java.util.Date;
 
 @Getter
@@ -14,15 +15,20 @@ import java.util.Date;
 public class BoardCommentResponse {
     private Long comment_id;
     private String user_name;
-    private Date create_date;
+    private String create_date;
     private String text;
     private Integer replyCount;
 
     public BoardCommentResponse(BoardComment comment) {
         this.comment_id = comment.getBoardComment_id();
         this.user_name = comment.getUser().getUsername();
-        this.create_date = comment.getCreateDate();
+        this.create_date = asString(comment.getCreateDate());
         this.text = comment.getText();
         this.replyCount = comment.getReplyCnt();
+    }
+
+    private String asString(Date date){
+        SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
+        return format.format(date);
     }
 }
