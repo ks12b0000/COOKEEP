219d8f8f9a0a2c64bf1af9a6f1dfe8de0540934f
ks12b0000 <ks12b0000@gmail.com>
2023-01-14 16:35:23 +0900

    로그인 시 응답값 username 추가

--- backend/src/main/java/teamproject/backend/user/dto/LoginResponse.java
+++ backend/src/main/java/teamproject/backend/user/dto/LoginResponse.java
@@ -3,6 +3,7 @@ package teamproject.backend.user.dto;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import teamproject.backend.domain.User;
 
 @Getter
 @NoArgsConstructor
@@ -11,4 +12,10 @@ public class LoginResponse {
 
     // 로그인 성공시 id값 받아옴.
     private Long id;
+
+    private String username;
+
+    public LoginResponse(Long user_id) {
+        this.id = user_id;
+    }
 }
