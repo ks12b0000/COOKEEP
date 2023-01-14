a0eb527da9b388d5a97ba05022c3d786210581ce
ks12b0000 <ks12b0000@gmail.com>
2023-01-14 16:38:22 +0900

    로그인 시 응답값 username 추가

--- backend/src/main/java/teamproject/backend/config/filter/AuthorizationFilter.java
+++ backend/src/main/java/teamproject/backend/config/filter/AuthorizationFilter.java
@@ -42,7 +42,6 @@ public class AuthorizationFilter implements Filter {
         HttpServletRequest httpReq = (HttpServletRequest) request;
         // 전체 쿠키 목록 가져오기
         Cookie[] cookies = httpReq.getCookies();
-        log.info("cookies = {}", cookies);
 
         Cookie accessCookie = cookieService.findCookie("accessToken", cookies);
         Cookie refreshCookie = cookieService.findCookie("refreshToken", cookies);
