a552c6d5bc42dca74d495f5a3a2c7394003f7296
ks12b0000 <ks12b0000@gmail.com>
2023-01-15 18:24:30 +0900

    user 탈퇴 시 댓글 대댓글 삭제 추가

--- backend/src/main/java/teamproject/backend/domain/BoardCommentReply.java
+++ backend/src/main/java/teamproject/backend/domain/BoardCommentReply.java
@@ -2,6 +2,8 @@ package teamproject.backend.domain;
 
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
 
 import javax.persistence.*;
 import java.util.Date;
@@ -19,11 +21,13 @@ public class BoardCommentReply {
     //댓글 작성자, FK(외래키 - User)
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "user_id")
+    @OnDelete(action = OnDeleteAction.CASCADE)
     private User user;
 
     //댓글이 작성된 글
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "boardComment_id")
+    @OnDelete(action = OnDeleteAction.CASCADE)
     private BoardComment boardComment;
 
     //댓글 내용
