6ad6149dec02f78d8ca2f731e69c32edf9190ffc
김성훈 <pole@1024corp.com>
2023-02-06 14:37:00 +0900

    수정

--- frontend/front/src/components/layout/home/Main.jsx
+++ frontend/front/src/components/layout/home/Main.jsx
@@ -1,8 +1,6 @@
 
 import Popularity from "./popularity/Popularity";
 import Ranking from "./ranking/Ranking";
-import Header from "../header/Header";
-import {color} from "../../../constants/color";
 
 
 function Main() {
