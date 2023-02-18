ac9c8c87c28e9577c021151923e58b4a1cc4ddfa
ChangDaeJun <eowns1209@gmail.com>
2023-02-18 17:05:51 +0900

    modify : 불필요한 코드 삭제(System.out.println)

--- src/main/java/teamproject/backend/imageFile/ImageDeleteScheduling.java
+++ src/main/java/teamproject/backend/imageFile/ImageDeleteScheduling.java
@@ -19,7 +19,6 @@ public class ImageDeleteScheduling {
     //@Scheduled(cron = "0 0/3 * 1/1 * ?")// 매 3분마다 - 테스트 시 사용
     @Scheduled(cron = "0 0 2 1/1 * ? *") // 매일 새벽 2시
     public void deleteUnusedImages(){
-        System.out.println("시작");
         Date ago6Hours = getAgoHours(6);
         //생성일로부터 6시간 이상 경과했으면서 동시에 board_id가 null인 이미지를 모두 찾음
         List<ImageFile> unmanagedImages = imageFileRepository.findByBoardIdIsNullAndCreateDateBefore(ago6Hours);
@@ -30,12 +29,10 @@ public class ImageDeleteScheduling {
 
             if(usedBoard == null){
                 //이미지가 사용 중이지 않다면, 이미지를 삭제한다.(DB, S3)
-                System.out.println(imageFile.getUrl() +"is delete");
                 imageFileService.delete(imageFile.getFileName());
             }
             else{
                 //이미지가 사용 중이라면 board_id를 넣어준다.
-                System.out.println(imageFile.getUrl() +" is " + usedBoard.getBoardId() + " image");
                 imageFile.setBoardId(usedBoard.getBoardId());
             }
         }
