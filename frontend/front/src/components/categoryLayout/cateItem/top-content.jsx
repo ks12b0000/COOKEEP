e131fa4e9faff60c009e85ba1859cae23638c024
uiux-ksh <uiux-ksh@naver.com>
2023-02-19 10:45:56 +0900

    머지부분하기

---
+++ frontend/front/src/components/categoryLayout/cateItem/top-content.jsx
@@ -0,0 +1,113 @@
+import styled from "@emotion/styled";
+
+function TopContent({detailPost}) {
+    const dt = new Date(detailPost?.create_date);
+    const DateString =dt.toLocaleDateString()
+    return(
+        <TopContainer>
+            <TopHeader>
+                <h1>{detailPost.category}</h1>
+                <h2>{detailPost.title}</h2>
+                <div>
+                    <span>{DateString}</span><span>View 111</span>
+                </div>
+                <h3>{detailPost.user_name}</h3>
+                <TopHeaderIconWrap>
+                    <li>
+                        <p><img src={`${process.env.PUBLIC_URL}/image/like.png`} alt=""/></p>
+                        <p>{detailPost.liked}</p>
+                    </li>
+                    <li>
+                        <p><img src={`${process.env.PUBLIC_URL}/image/message-dots-circle.png`} alt=""/></p>
+                        <p>{detailPost.commented}</p>
+                    </li>
+
+                </TopHeaderIconWrap>
+
+            </TopHeader>
+            <TopBody >
+                <div dangerouslySetInnerHTML={{__html: detailPost.text}}/>
+            </TopBody>
+        </TopContainer>
+    )
+}
+export default TopContent;
+
+const TopContainer = styled.div`
+    margin:24px auto;
+    width: 100%;
+    padding:24px;
+    box-sizing: border-box;
+    border: 1px solid #FFA590;
+    border-radius: 20px;
+
+  
+`;
+const TopHeader = styled.div`
+  position: relative;
+  //카테고리
+  h1{
+    font-weight: 500;
+    font-size: 18px;
+    line-height: 23px;
+    color: #FF4122;
+
+  }
+  //타이틀
+  h2{
+    margin-top:9px;
+    font-style: normal;
+    font-weight: 700;
+    font-size: 24px;
+    line-height: 29px;
+    color: #000000;
+  }
+  //작성자
+  h3{
+    font-style: normal;
+    font-weight: 400;
+    font-size: 16px;
+    line-height: 23px;
+    color: #B0B0B0;
+  }
+  >div{
+    margin:8px 0;
+    display: flex;
+    gap:16px;
+    span{
+      display: block;
+      font-style: normal;
+      font-weight: 400;
+      font-size: 12px;
+      line-height: 14px;
+      color: #B0B0B0;
+    }
+  }
+`
+const TopHeaderIconWrap = styled.ul`
+  display: flex;
+  text-align: center;
+  gap:14px;
+  position: absolute; 
+  right:0;
+  top:0;
+  color: #B0B0B0;
+  li{
+    p{
+      font-size: 12px;
+    }
+  }
+`
+const TopBody = styled.div`
+  margin:16px auto 0;
+  padding-top:16px;
+  box-sizing: border-box;
+  border-top: 1px solid #CED4DA;;
+  text-align: center;
+  min-height: 750px;
+  font-weight: 400;
+  font-size: 16px;
+  line-height: 23px;
+  color: #3E4145;
+
+`
--- frontend/front/src/components/categoryLayout/detail/Detail.jsx
+++ frontend/front/src/components/categoryLayout/detail/Detail.jsx
@@ -15,6 +15,7 @@ import {useNavigate} from "react-router";
 import Header from "../../layout/header/Header";
 import Footer from "../../layout/footer/Footer";
 import Banner from "../../layout/home/banner/Banner";
+import TopContent from "../cateItem/top-content";
 
 function Detail() {
     const categoryHttp = new CategoryHttp();
@@ -25,10 +26,12 @@ function Detail() {
 
     const {userId} = useSelector(state => state.persistedReducer.userReducer);
     const [detailUserId,setDetailUserId] = useState(null);
+
     const { id } = useParams();
     const [detailPost, setDetailPost] = useState([]);
-    const {isOpen,controller} = useModal();
 
+    const {isOpen,controller} = useModal();
+     console.log(detailPost);
     const FetchDelete = async () => {
         try {
             await categoryHttp.deleteCategoryList(id,userId);
@@ -73,21 +76,13 @@ function Detail() {
             <Banner />
 
             <Container>
-                <Top>
-                    <TopImg>
-                        <img src={detailPost.thumbnail} alt="" />
-                    </TopImg>
-                    <TopText>
-                        <TextTitle>{detailPost.title}</TextTitle>
-                        <Text dangerouslySetInnerHTML={{ __html: detailPost.text }}></Text>
-                        <LikeButton onClick={(e)=>onLike(e)}>좋아요</LikeButton>
-                    </TopText>
-                </Top>
-                {detailUserId === userId ?
-                <ButtonWrap>
-                  <ButtonStyle onClick={controller}>삭제</ButtonStyle>
-                  <ButtonStyle onClick={() => navigate(`/${id}/edit`)}>수정</ButtonStyle>
-                </ButtonWrap>  : null}
+
+                <TopContent detailPost={detailPost} />
+                {/*{detailUserId === userId ?*/}
+                {/*<ButtonWrap>*/}
+                {/*  <ButtonStyle onClick={controller}>삭제</ButtonStyle>*/}
+                {/*  <ButtonStyle onClick={() => navigate(`/${id}/edit`)}>수정</ButtonStyle>*/}
+                {/*</ButtonWrap>  : null}*/}
                 <CommentUpload boardId={id} />
                 <CommentList boardId={id} />
             </Container>
@@ -99,75 +94,10 @@ function Detail() {
 export default Detail;
 
 const Container = styled.article`
-    width: 1100px;
-    margin: 100px auto 0;
-`;
-const Top = styled.div`
-    display: flex;
-    justify-content: space-between;
-`;
-const TopImg = styled.figure`
-    width: 50%;
-    height: 500px;
-    img {
-        width: 100%;
-        height: 100%;
-        object-fit: cover;
-    }
-`;
-const TopText = styled.figcaption`
-    width: 50%;
-    height: 500px;
-    border: 1px solid #eee;
-    box-sizing: border-box;
-    padding: 1rem 0 0;
-    span {
-        display: block;
-        padding: 0 1rem;
-        box-sizing: border-box;
-    }
-`;
-const TextTitle = styled.span`
-    font-size: 24px;
-    font-weight: 600;
-    height: 10%;
-`;
-const Text = styled.span`
-    font-size: 18px;
-    height: 80%;
-    font-weight: 500;
-`;
-const LikeButton = styled.button`
-    cursor: pointer;
+    width: 1440px;
+    margin: 0 auto ;
 
-    background: #222;
-    height: 10%;
-    width: 100%;
-    color: wheat;
-    transition: 0.5s;
-    &:hover {
-        background: #333;
-    }
+ 
 `;
-const ButtonWrap = styled.div`
- display: flex;
-  justify-content: end;
-  gap:15px;
-`
-const ButtonStyle = styled.button`
-  
-  margin-top:10px;
-  width: 100px;
-  height: 50px;
-  border:1px solid #ccc;
-  background:#ffffff;
-  cursor: pointer;
-  border-radius: 3px;
-  &:hover{
-    background:#222;
-    color:#ffffff;
-  }
-  &:focus{
-    border:1px solid #ccc;
-  }
-`
+
+
