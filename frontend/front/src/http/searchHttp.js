f9e5ed2a4ffc4642c712a386026c2ff723fff4fd
uiux-ksh <uiux-ksh@naver.com>
2023-03-26 09:58:51 +0900

    태그검색

--- frontend/front/src/http/searchHttp.js
+++ frontend/front/src/http/searchHttp.js
@@ -3,6 +3,25 @@
 import Http from "./http";
 
 class SearchHttp extends Http {
+
+    //자동 검색어
+    getAutoSearch = async (text) => {
+        try{
+            return await  this.axios.get(`/main/auto/search/list?keyword=${text}`)
+        }
+        catch (err) {
+            throw  err;
+        }
+    }
+
+    getAutoTag= async (text) => {
+        try{
+            return await  this.axios.get(`/main/search/tag/list?keyword=${text}`)
+        }
+        catch (err) {
+            throw  err;
+        }
+    }
     //메인 탑 태그 검색
     getTopTag = async () => {
         try{
