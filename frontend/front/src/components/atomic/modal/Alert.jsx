f142fd0690abd6505faa11d632f19b20e889c7e9
김성훈 <pole@1024corp.com>
2023-04-04 15:03:49 +0900

    모달 컴포넌트 만들기

--- frontend/front/src/components/atomic/modal/Alert.jsx
+++ frontend/front/src/components/atomic/modal/Alert.jsx
@@ -1,32 +1,16 @@
 import styled from "@emotion/styled";
-function    Alert({ setOpenModal,onClick,text }) {
+import ModalBtn from "./ModalBtn";
+import ModalBody from "./ModalBody";
+function Alert({ setOpenModal,buttons,body }) {
+
+
+
     return (
         <>
-        <ModalBackground  onClick={setOpenModal}>
-        </ModalBackground>
+        <ModalBackground  onClick={setOpenModal} />
             <ModalContainer>
-                <TitleCloseBtn>
-                    <button
-                        onClick={setOpenModal}
-                    >
-                        X
-                    </button>
-                </TitleCloseBtn>
-                <Title>
-                    <h1>알림창</h1>
-                </Title>
-                <ModalBody>
-                    <p>{text}</p>
-                </ModalBody>
-                <ModalFooter>
-                    <button
-                        onClick={setOpenModal}
-                        id="cancelBtn"
-                    >
-                        취소
-                    </button>
-                    <button onClick={onClick}>확인</button>
-                </ModalFooter>
+                <ModalBody {...body} />
+                <ModalBtn  {...buttons} />
             </ModalContainer>
         </>
 
@@ -44,91 +28,23 @@ const ModalBackground = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
+  z-index: 100;
 `
 const ModalContainer = styled.div`
   position: fixed;
   top:50%;
   left:50%;
   transform: translate(-50%,-50%);
-  width: 400px;
-  max-height: 500px;
-  border-radius: 5px;
-  border: 1px solid  #FF4122;
+  width: 500px;
+  height: 256px;
+  border-radius: 10px;
+  border-bottom: 1px solid #CED4DA;
   background-color: white;
-  box-shadow: 0px 4px 12px  rgba(255,65,34,.15);
   display: flex;
   flex-direction: column;
-  padding: 0 25px 15px ;
+
   z-index: 1000;
 `
 
-const  Title =styled.div`
- padding:15px 0;
-  display: inline-block;
-  text-align: left;
-  border-bottom: 1px solid #FF4122;
-  
-  h1{
-    font-size: 18px;
-  }
-`
 
-const TitleCloseBtn = styled.div`
-  position: absolute;
-  right:20px;
-  top:15px;
-  button{
-    width: 25px;
-    height: 25px;   
-    background-color: transparent;
-    border: none;
-    font-size: 20px;
-    cursor: pointer;
-    color:#FF4122;
 
-    &:hover{
-      display: block;
-      width: 25px;
-      height: 25px;
-      color:#222222;
-   
-    }
-  }
-`
-const ModalBody = styled.div`
-  flex: 50%;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-  min-height: 120px;
-  >p {
-    color: #FF4122;
-    font-size: 16px;
-  }
-`
-
-const ModalFooter = styled.div`
-  flex: 20%;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  
-  button{
-    width: 100px;
-    height: 40px;
-    margin: 10px;
-    background-color: white;
-    color: #FF4122;
-    border:1px solid  #FF4122;
-    border-radius: 8px;
-    font-size: 14px;
-    cursor: pointer;
-    
-    &:hover{
-      background: #FF4122;
-      color:#ffffff;
-    }
-  }
-  
-  `
---
+++ frontend/front/src/components/atomic/modal/ModalBtn.jsx
@@ -0,0 +1,53 @@
+import styled from '@emotion/styled'
+
+
+
+
+function ModalBtn({btn}) {
+
+
+
+    return (
+          <ModalFooter btn ={btn}>
+              {btn.map((item,index) => (
+                  <button key={index} onClick={item.onClick}> {item.text}</button>
+              ))}
+          </ModalFooter>
+    )
+}
+export default  ModalBtn;
+
+const ModalFooter = styled.div`
+      width: 100%;
+      border-top:1px solid #CED4DA;
+      display: flex;
+      height: 56px;
+      justify-content: center;
+      align-items: center;
+      position: relative;
+
+      &:before {
+        display: ${props => props.btn.length === 1 ?  'none' : 'block'};
+        width: 1px;
+        height: 56px;
+        background:#CED4DA;
+        position: absolute;
+        top:0;
+        left:50%;
+        content:"";
+      }
+      button{
+        width: ${props=> props.btn.length === 1 ?  '100%' : '50%'};
+        height: 100%;
+        border:none;
+        background:white;
+        border-radius: 10px ;
+        color:#5A5C5F;
+        font-size: 14px;
+        font-weight: 700;
+        cursor: pointer;
+        &:hover {
+          color: #FF4122;
+        }
+      }
+    `
