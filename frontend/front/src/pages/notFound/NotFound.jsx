c1b05fbe8525b922e9725f66cc721e43a32a8d78
mim-mel <mfblade2014@gmail.com>
2023-04-30 18:09:54 +0900

    feat: notfound 페이지 완성

--- frontend/front/src/pages/notFound/NotFound.jsx
+++ frontend/front/src/pages/notFound/NotFound.jsx
@@ -1,9 +1,43 @@
+import styled from '@emotion/styled';
+import Layout from '../../components/layout/Layout';
+
+
 const NotFound = () => {
- return(
-    <>
-    <div>존재하지 않는 페이지 입니다.</div>
-    </>
+   return (
+    <Layout>
+         <Wrap>
+            <ContentsWrap>
+               <Img src='/image/notfound.png' />
+               <Text>존재하지 않는 페이지 입니다.</Text>
+            </ContentsWrap>
+      </Wrap>
+   </Layout>
  )
 }
 
+const Wrap = styled.div`
+   width: 100vw;
+   height: 84vh;
+   display: flex;
+   justify-content: center;
+   align-items: center;
+   padding-bottom: 40px;
+   box-sizing: border-box;
+`
+
+const ContentsWrap = styled.div`
+   display: flex;
+   flex-direction: column;
+   justify-content: center;
+   align-items: center;
+`
+
+const Img = styled.img`
+   margin-bottom: 15px;
+`
+
+const Text = styled.div`
+   font-size: 18px;
+   font-weight: 700;
+`
 export default NotFound;
