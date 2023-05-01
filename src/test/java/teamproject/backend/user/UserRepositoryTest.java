424078c8d3a6b20f3971b6856090e2b5a0e4256f
ks12b0000 <ks12b0000@gmail.com>
2023-05-01 22:54:41 +0900

    UserRepository TestCode 작성중

--- src/test/java/teamproject/backend/user/UserRepositoryTest.java
+++ src/test/java/teamproject/backend/user/UserRepositoryTest.java
@@ -1,62 +1,118 @@
 package teamproject.backend.user;
 
 import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Named;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.dao.DataIntegrityViolationException;
 import teamproject.backend.domain.User;
+import teamproject.backend.response.BaseException;
+
+import java.util.Optional;
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Named.*;
 
 @Slf4j
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 @DataJpaTest
-public class UserRepositoryTest {
+class UserRepositoryTest {
 
     @Autowired
     private UserRepository userRepository;
 
+    private static final User user = new User("userName1", "userNickName1","userEmail1@gmail.com","test123422", null);
+
     @Test
-    public void join_Test() {
+    @DisplayName("유저 회원가입")
+    void join() {
         // given
-        User user = new User("test1234", "test1234@gmail.com", "test1234");
 
         // when
-        User userPS = userRepository.save(user);
+        User savedUser = userRepository.save(user);
 
         // then
-        assertThat(user.getUsername()).isEqualTo(userPS.getUsername());
-        assertThat(user.getEmail()).isEqualTo(userPS.getEmail());
-        assertThat(user.getPassword()).isEqualTo(userPS.getPassword());
+        assertAll(
+                () -> assertThat(savedUser.getUsername()).isEqualTo("userName1"),
+                () -> assertThat(savedUser.getNickname()).isEqualTo("userNickName1"),
+                () -> assertThat(savedUser.getEmail()).isEqualTo("userEmail1@gmail.com"),
+                () -> assertThat(savedUser.getPassword()).isEqualTo("test123422")
+        );
     }
 
     @Test
-    public void join_중복Test() {
+    @DisplayName("회원가입된 유저를 아이디로 조회한다.")
+    void findById() {
         // given
-        User user = new User("test1234", "test1234@gmail.com", "test1234");
+        User savedUser = userRepository.save(user);
 
+        // when
+        Optional<User> findUser = userRepository.findById(savedUser.getId());
+
+        // then
+        assertAll(
+                () -> assertThat(findUser.isPresent()),
+                () -> assertThat(findUser.get()).isEqualTo(savedUser)
+        );
+    }
+
+    @Test
+    @DisplayName("회원가입된 유저를 유저네임으로 조회한다.")
+    void findByUserName() {
+        // given
+        User savedUser = userRepository.save(user);
+
+        // when
+        Optional<User> findUser = Optional.ofNullable(userRepository.findByUsername(savedUser.getUsername()));
+
+        // then
+        assertAll(
+                () -> assertThat(findUser.isPresent()),
+                () -> assertThat(findUser.get()).isEqualTo(savedUser)
+        );
+    }
+
+    @ParameterizedTest(name = "{0}")
+    @MethodSource("duplicate")
+    @DisplayName("중복된 아이디, 이메일, 닉네임으로 회원가입을 할 수 없다.")
+    void duplicateUserNameAndEmailAndNickName(User user2) {
+        // given
         userRepository.save(user);
 
         // when
-        User user2 = new User("test1234", "test1234@gmail.com", "test1234");
-
         // then
         assertThrows(RuntimeException.class, () -> userRepository.save(user2));
     }
+    private static Stream<Arguments> duplicate() {
+        return Stream.of(
+                Arguments.of(named("아이디 중복", new User("userName1", "userNickName2","userEmail2@gmail.com","test123422", null))),
+                Arguments.of(named("이메일 중복", new User("userName2", "userNickName2","userEmail1@gmail.com","test123422", null))),
+                Arguments.of(named("닉네임 중복", new User("userName2", "userNickName1","userEmail2@gmail.com","test123422", null)))
+        );
+    }
 
     @Test
-    public void login_Test() {
+    @DisplayName("UserName과 PassWord가 일치하는 유저를 조회한다.")
+    void findByUserNameAndPassWord() {
         // given
-        User user = new User("test1234", "test1234@gmail.com", "test1234");
         userRepository.save(user);
 
         // when
-        User userLogin = userRepository.findByUsernameAndPassword(user.getUsername(), user.getPassword());
+        Optional<User> findUser = Optional.ofNullable(userRepository.findByUsernameAndPassword(user.getUsername(), user.getPassword()));
 
         // then
-        assertThat(user.getUsername()).isEqualTo(userLogin.getUsername());
-        assertThat(user.getPassword()).isEqualTo(userLogin.getPassword());
+        assertAll(
+                () -> assertThat(findUser.isPresent()),
+                () -> assertThat(findUser.get()).isEqualTo(user)
+        );
     }
 }
