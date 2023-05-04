a0eb6f67b80f4cc35dfca09bada44f91447f478f
ks12b0000 <ks12b0000@gmail.com>
2023-05-04 18:15:03 +0900

    테스트 코드 작성중 && user 일반, 소셜 login 수정

--- src/test/java/teamproject/backend/user/UserRepositoryTest.java
+++ src/test/java/teamproject/backend/user/UserRepositoryTest.java
@@ -57,11 +57,14 @@ class UserRepositoryTest {
 
         // when
         Optional<User> findUser = userRepository.findById(savedUser.getId());
+        Optional<User> findUser2 = userRepository.findById(2L);
 
         // then
         assertAll(
                 () -> assertThat(findUser.isPresent()),
-                () -> assertThat(findUser.get()).isEqualTo(savedUser)
+                () -> assertThat(findUser.get()).isEqualTo(savedUser),
+
+                () -> assertThat(findUser2.isPresent()).isFalse()
         );
     }
 
@@ -73,11 +76,52 @@ class UserRepositoryTest {
 
         // when
         Optional<User> findUser = Optional.ofNullable(userRepository.findByUsername(savedUser.getUsername()));
+        Optional<User> findUser2 = Optional.ofNullable(userRepository.findByUsername("dddfe211"));
 
         // then
         assertAll(
                 () -> assertThat(findUser.isPresent()),
-                () -> assertThat(findUser.get()).isEqualTo(savedUser)
+                () -> assertThat(findUser.get()).isEqualTo(savedUser),
+
+                () -> assertThat(findUser2.isPresent()).isFalse()
+        );
+    }
+
+    @Test
+    @DisplayName("회원가입된 유저를 이메일로 조회한다.")
+    void findByEmail() {
+        // given
+        User savedUser = userRepository.save(user);
+
+        // when
+        Optional<User> findUser = Optional.ofNullable(userRepository.findByEmail(savedUser.getEmail()));
+        Optional<User> findUser2 = Optional.ofNullable(userRepository.findByEmail("ksadasd@gmail.com"));
+
+        // then
+        assertAll(
+                () -> assertThat(findUser.isPresent()),
+                () -> assertThat(findUser.get()).isEqualTo(savedUser),
+
+                () -> assertThat(findUser2.isPresent()).isFalse()
+        );
+    }
+
+    @Test
+    @DisplayName("회원가입된 유저를 유저네임과 이메일로 조회한다.")
+    void findByUserNameAndEmail() {
+        // given
+        User savedUser = userRepository.save(user);
+
+        // when
+        Optional<User> findUser = Optional.ofNullable(userRepository.findByUsernameAndEmail(savedUser.getUsername(), savedUser.getEmail()));
+        Optional<User> findUser2 = Optional.ofNullable(userRepository.findByUsernameAndEmail("username222","ksadasd@gmail.com"));
+
+        // then
+        assertAll(
+                () -> assertThat(findUser.isPresent()),
+                () -> assertThat(findUser.get()).isEqualTo(savedUser),
+
+                () -> assertThat(findUser2.isPresent()).isFalse()
         );
     }
 
@@ -113,6 +157,46 @@ class UserRepositoryTest {
         assertAll(
                 () -> assertThat(findUser.isPresent()),
                 () -> assertThat(findUser.get()).isEqualTo(user)
+        );
+    }
+
+    @Test
+    @DisplayName("UserNickName이 사용 중인지 확인.")
+    void existsUserByNickname() {
+        // given
+        userRepository.save(user);
+
+        // when
+        boolean findUser = userRepository.existsUserByNickname("userNickName1");
+        boolean findUser2 = userRepository.existsUserByNickname("userNickName12");
+
+        // then
+        assertThat(findUser);
+        assertThat(!findUser2);
+    }
+
+
+    @ParameterizedTest(name = "{0}")
+    @MethodSource("validation")
+    @DisplayName("아이디, 닉네임, 이메일, 비밀번호가 null 이거나 지정 된 길이보다 넘어갈 시 에러가 발생한다.")
+    void validationCheck(User user2) {
+        // given
+
+        // when
+        // then
+        assertThrows(RuntimeException.class, () -> userRepository.save(user2));
+    }
+    private static Stream<Arguments> validation() {
+        return Stream.of(
+                Arguments.of(named("아이디가 null일 때 에러 발생", new User(null, "userNickName2","userEmail2@gmail.com","test123422", null))),
+                Arguments.of(named("아이디가 50자 이상 넘어갈 때 에러발생", new User("asdasdfasdasdfasdasdfasdasdfasdasdfasdasdfasdasdfasdasdfasdasdfasdasdf", "userNickName2","userEmail2@gmail.com","test123422", null))),
+
+                Arguments.of(named("닉네임이 null일 때 에러 발생", new User("userName2", null,"userEmail1@gmail.com","test123422", null))),
+                Arguments.of(named("닉네임이 50자 이상 넘어갈 때 에러발생", new User("userName22", "userNickName3userNickName3userNickName3userNickName3userNickName3userNickName3userNickName3","userEmail2@gmail.com","test123422", null))),
+
+                Arguments.of(named("이메일이 null일 때 에러 발생", new User("userName243", "userNickName234",null,"test123422", null))),
+
+                Arguments.of(named("비밀번호가 null일 때 에러 발생", new User("userName2112", "userNickName23412","userEmail1",null, null)))
         );
     }
 }
