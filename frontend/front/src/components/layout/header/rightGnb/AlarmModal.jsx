598e4d24d3db6ad4ffb956cb8ae568bda6f2e5cd
mim-mel <mfblade2014@gmail.com>
2023-06-06 21:17:48 +0900

    feat: 메인페이지 알림리스트 기능 완성

--- frontend/front/src/components/layout/header/rightGnb/AlarmModal.jsx
+++ frontend/front/src/components/layout/header/rightGnb/AlarmModal.jsx
@@ -1,22 +1,87 @@
 import styled from '@emotion/styled';
+import { useState, useEffect } from 'react';
+import { useNavigate } from 'react-router';
+import AuthHttp from '../../../../http/authHttp';
+import { useSelector } from 'react-redux';
+
+const authHttp = new AuthHttp();
 
 const AlarmModal = () => {
+  const userId = useSelector(
+    state => state.persistedReducer.userReducer.userId
+  );
+
+  const navigate = useNavigate();
+
+  const [Alarms, setAlarms] = useState([]);
+  const [Page, setPage] = useState(0);
+  const [Total, setTotal] = useState(0);
+
+  useEffect(() => {
+    getAlarmList();
+  }, [Page]);
+
+  const getAlarmList = async () => {
+    try {
+      const res = await authHttp.getMainAlarmList(userId, Page);
+      console.log(res);
+      const newAlarms = [...Alarms, ...res.data.result.notificationList];
+      setAlarms(newAlarms);
+      setTotal(res.data.result.total);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  const addPage = e => {
+    e.preventDefault();
+    if (Page < Total - 1) {
+      setPage(prev => prev + 1);
+    }
+  };
+
+  //오늘 날짜 구하기
+  const currentDate = new Date();
+
+  const year = currentDate.getFullYear(); // 년도
+  const month = String(currentDate.getMonth() + 1).padStart(2, '0'); // 월 (0부터 시작하므로 +1 필요)
+  const day = String(currentDate.getDate()).padStart(2, '0'); // 일
+
+  const formattedDate = `${year}.${month}.${day}`;
+
   return (
     <ModalWrap>
-      <ContentsBox>
-        <RedRound />
-        <TextWrap>
-          <TitleWrap>
-            <WelcomeImg src='/image/welcome.png' />
-            <Title>회원가입을 환영합니다!</Title>
-          </TitleWrap>
-          <SubTitle>지금 닉네임을 변경하고 활동을 시작해 보세요!</SubTitle>
-          <Time>00:00</Time>
-        </TextWrap>
-        {/* <ArrowWrap> */}
-        <Arrow src='/image/alarm-arrow.png' />
-        {/* </ArrowWrap> */}
-      </ContentsBox>
+      <ConentsWrap>
+        {Alarms.map(alarm => (
+          <ContentsBox key={alarm.notification_id}>
+            <RedRound />
+            <TextWrap>
+              <TitleWrap>
+                <Title>{alarm.title}</Title>
+              </TitleWrap>
+              <SubTitle>{alarm.subTitle}</SubTitle>
+              {formattedDate === alarm.createDate ? (
+                <Time>{alarm.createTime}</Time>
+              ) : (
+                <Time>{alarm.createDate}</Time>
+              )}
+            </TextWrap>
+            {/* <ArrowWrap> */}
+            <Arrow
+              src='/image/alarm-arrow.png'
+              onClick={() => {
+                window.open(alarm.notification_url, '_self');
+              }}
+            />
+            {/* </ArrowWrap> */}
+          </ContentsBox>
+        ))}
+        {Page === Total - 1 ? (
+          <></>
+        ) : (
+          <SeeMore onClick={e => addPage(e)}>더보기</SeeMore>
+        )}
+      </ConentsWrap>
     </ModalWrap>
   );
 };
@@ -26,26 +91,37 @@ const ModalWrap = styled.div`
   top: 60px;
   z-index: 1000;
   left: -300px;
+  height: 550px;
   width: 400px;
-  height: auto;
+  /* overflow-y: scroll; */
   background: #ffffff;
   border-radius: 5px;
   border: 1px solid #ff4122;
   justify-content: center;
   display: flex;
-  padding: 22px 0px;
   align-items: center;
   flex-direction: column;
   text-align: center;
 `;
 
+const ConentsWrap = styled.div`
+  position: relative;
+  width: 100%;
+  height: 100%;
+  overflow-y: scroll;
+  top: 0;
+  left: 0;
+`;
 const ContentsBox = styled.div`
   width: 370px;
-  margin: 0 auto;
+  margin: 0px auto;
   display: grid;
   grid-template-columns: 5% 80% 5%;
   justify-content: space-between;
   align-items: center;
+  padding: 20px 5px;
+  box-sizing: border-box;
+  border-bottom: 0.1px solid rgba(0, 0, 0, 0.2);
 `;
 
 const RedRound = styled.div`
@@ -71,14 +147,16 @@ const WelcomeImg = styled.img`
 `;
 
 const Title = styled.div`
-  font-size: 18px;
+  font-size: 16px;
   font-weight: 600;
+  text-align: left;
+  margin-bottom: 3px;
 `;
 
 const SubTitle = styled.div`
   font-size: 12px;
   color: #838485;
-  margin-bottom: 16px;
+  margin-bottom: 12px;
   text-align: left;
 `;
 
@@ -91,6 +169,16 @@ const Time = styled.div`
 const Arrow = styled.img`
   width: 8px !important ;
   height: 14px !important ;
+`;
+
+const SeeMore = styled.div`
+  font-size: 16px;
+  color: #5a5c5f;
+  width: 100%;
+  text-align: center;
+  margin: 30px 0;
+  text-decoration: underline;
+  cursor: pointer;
 `;
 
 export default AlarmModal;
