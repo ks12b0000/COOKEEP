df5856ac02d93c99e1d5c57733ba895f825847d6
ks12b0000 <ks12b0000@gmail.com>
2023-12-08 15:25:27 +0900

    refactor: URL 변경

--- frontend/front/src/http/http.js
+++ frontend/front/src/http/http.js
@@ -1,34 +1,37 @@
 import axios from "axios";
 class Http {
-    constructor() {
-        this.axios = Http.createAxios();
-        const transformResponse = Http.transformResponse();
-    }
-    static createAxios() {
-        return axios.create({
-            baseURL: "https://www.teamprojectvv.shop",
-            timeout: 30 * 1000,
-            withCredentials: true,
-            headers: {
-                "Access-Control-Allow-Origin": "https://www.teamprojectvv.shop"
-            }
-        });
-    }
+  constructor() {
+    this.axios = Http.createAxios();
+    const transformResponse = Http.transformResponse();
+  }
+  static createAxios() {
+    return axios.create({
+      baseURL: "http://13.124.82.115:8080",
+      timeout: 30 * 1000,
+      withCredentials: true,
+      headers: {
+        "Access-Control-Allow-Origin": "http://13.124.82.115:8080",
+      },
+    });
+  }
 
-    static transformResponse() {
-        return {
-            onFulfilled: (response) => {
-                response.data = {
-                    statusCode: response.status,
-                    jsonResult: response.status === 200 || response.status === 201 ? response.data : null
-                };
-                return response;
-            },
-            onRejected: (error) => {
-                return Promise.reject(error);
-            }
+  static transformResponse() {
+    return {
+      onFulfilled: (response) => {
+        response.data = {
+          statusCode: response.status,
+          jsonResult:
+            response.status === 200 || response.status === 201
+              ? response.data
+              : null,
         };
-    }
+        return response;
+      },
+      onRejected: (error) => {
+        return Promise.reject(error);
+      },
+    };
+  }
 }
 
 export default Http;
